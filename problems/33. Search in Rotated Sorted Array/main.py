2ce217e00c811041097e27f88a93de26bcd35a41
Tycho Lin <tycho.lin@gmail.com>
2026-05-13 16:27:01 +0800

    Add 153. Add 162. Add 33.

---
+++ problems/33. Search in Rotated Sorted Array/main.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if target == nums[mid]:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if target >= nums[low] and target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if target > nums[mid] and target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+        return -1
